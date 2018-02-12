0374fe4c8a0b648b7f4f1d4f94503e02de663516
kimullamen <pbrehpuum@gmail.com>
2018-02-12 13:18:12 +0900

    リストの追加・編集のスタイルを変更

--- src/components/ListEditForm.vue
+++ src/components/ListEditForm.vue
@@ -1,45 +1,62 @@
 <template>
   <v-card>
-    <v-card-title>リストを編集する</v-card-title>
-    <v-card-text>
-      <v-form v-model="valid" ref="form" lazy-validation @submit.prevent="submit">
-        <v-flex d-flex>
-          <v-text-field
-          label="Name"
-          v-model="name"
-          :rules="nameRules"
-          required
-          ></v-text-field>
-        </v-flex>
-        <v-flex d-flex>
-          <icon-picker v-model="icon">
-            <v-text-field
-            name="icon"
-            label="Icon"
-            v-model="icon"
-            :rules="iconRules"
-            required
-            ></v-text-field>
-          </icon-picker>
-        </v-flex>
-        <v-flex d-flex>
-          <color-picker v-model="color">
-            <v-text-field
-            name="color"
-            label="Color"
-            v-model="color"
-            :rules="colorRules"
-            required
-            ></v-text-field>
-          </color-picker>
-        </v-flex>
-        <v-flex class="mb-3">
-          <label>Icon Preview</label>
-          <v-icon large class="ml-3" :color="color">{{icon}}</v-icon>
-        </v-flex>
-        <v-btn type="submit" color="primary">Save</v-btn>
-      </v-form>
-    </v-card-text>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-card-title>リストを編集する</v-card-title>
+        <v-card-text>
+          <v-form v-model="valid" ref="form" lazy-validation @submit.prevent="submit">
+            <v-layout row wrap>
+              <v-flex xs12>
+                <v-text-field
+                label="Name"
+                v-model="name"
+                :rules="nameRules"
+                required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+
+            <v-layout row wrap>
+              <v-flex xs7>
+                <v-layout row wrap>
+                  <v-flex d-flex>
+                    <icon-picker v-model="icon">
+                      <v-text-field
+                      name="icon"
+                      label="Icon"
+                      v-model="icon"
+                      :rules="iconRules"
+                      required
+                      ></v-text-field>
+                    </icon-picker>
+                  </v-flex>
+
+                  <v-flex d-flex>
+                    <color-picker v-model="color">
+                      <v-text-field
+                      name="color"
+                      label="Color"
+                      v-model="color"
+                      :rules="colorRules"
+                      required
+                      ></v-text-field>
+                    </color-picker>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+
+              <v-flex xs5 style="border-style:dotted">
+                <div class="text-xs-center" style="height:100%">
+                  <v-icon style="height:100%" large :color="color">{{icon}}</v-icon>
+                </div>
+              </v-flex>
+            </v-layout>
+
+            <v-btn type="submit" color="primary">Save</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-layout>
+    </v-container>
   </v-card>
 </template>
 
